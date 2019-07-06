b71112eba0734ad83257ef9db2ce82155fb948fd
Murtada al Mousawy <murtada.al.mousawy@gmail.com>
2019-07-06 13:25:12 +0200

    feat(build): Add JS gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp         = require('gulp'),
       del          = require('del'),
       scss         = require('gulp-sass'),
       autoprefixer = require('gulp-autoprefixer'),
+      concat       = require('gulp-concat'),
+      uglify       = require('gulp-uglify'),
+      pipeline     = require('readable-stream').pipeline,
       notify       = require('gulp-notify'),
       browserSync  = require('browser-sync').create();
 
@@ -10,59 +13,71 @@ const config = require('./gulp-config.json');
 
 // Clean
 function clean() {
-	return del(config.destination);
+  return del(config.destination);
 }
 
 // Images
 function imagesTask() {
   return gulp.src([
-		`${config.source}/images/**/*`,
-		`!${config.source}/images/.gitkeep`
-	], { base: config.source })
+    `${config.source}/images/**/*`,
+    `!${config.source}/images/.gitkeep`
+  ], { base: config.source })
   .pipe(gulp.dest(config.destination));
 }
 
 // HTML
 function htmlTask() {
-	return gulp.src(`${config.source}/*.html`, { base: config.source })
-	.pipe(gulp.dest(config.destination));
+  return gulp.src(`${config.source}/*.html`, { base: config.source })
+  .pipe(gulp.dest(config.destination));
 }
 
 // SCSS
 function scssTask() {
-	return gulp.src(`${config.source}/style.scss`, { base: `${config.source}` })
-	.pipe(scss())
-	.on('error', notify.onError('SCSS compile error: <%= error.message %>'))
-	.pipe(autoprefixer({ browsers: 'last 2 versions' }))
-	.pipe(gulp.dest(config.destination))
-	.pipe(browserSync.stream());
+  return gulp.src(`${config.source}/**/*.scss`, { base: `${config.source}` })
+  .pipe(scss())
+  .on('error', notify.onError('SCSS compile error: <%= error.message %>'))
+  .pipe(autoprefixer({ browsers: 'last 2 versions' }))
+  .pipe(gulp.dest(config.destination))
+  .pipe(browserSync.stream());
+}
+
+// JS
+function jsTask() {
+  return gulp.src([
+      `${config.source}/js/**/*.js`,
+    ], { base: config.source })
+    .pipe(concat('script.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(config.destination)
+  );
 }
 
 // Serve
 function serve(done) {
-	browserSync.init({
-		server: {
-			baseDir: config.destination
-		}
-	});
+  browserSync.init({
+    server: {
+      baseDir: config.destination
+    }
+  });
 
-	done();
+  done();
 }
 
 // BrowserSync reload
 function reloadTask(done) {
-	browserSync.reload();
+  browserSync.reload();
 
-	done();
+  done();
 }
 
 // Watch
 function watchTask() {
-	gulp.watch(`${config.source}/images/**/*`, gulp.series(imagesTask, reloadTask));
-	gulp.watch(`${config.source}/*.html`, gulp.series(htmlTask, reloadTask));
-	gulp.watch(`${config.source}/*.scss`, scssTask);
+  gulp.watch(`${config.source}/images/**/*`, gulp.series(imagesTask, reloadTask));
+  gulp.watch(`${config.source}/*.html`, gulp.series(htmlTask, reloadTask));
+  gulp.watch(`${config.source}/*.scss`, scssTask);
+  gulp.watch(`${config.source}/js/**/*.js`, jsTask);
 }
 
 // Default task
-gulp.task('default', gulp.series(clean, gulp.parallel(htmlTask, scssTask, imagesTask), gulp.parallel(serve, watchTask)));
-gulp.task('build', gulp.series(clean, gulp.parallel(htmlTask, scssTask, imagesTask)));
+gulp.task('default', gulp.series(clean, gulp.parallel(htmlTask, scssTask, imagesTask, jsTask), gulp.parallel(serve, watchTask)));
+gulp.task('build', gulp.series(clean, gulp.parallel(htmlTask, scssTask, imagesTask, jsTask)));
